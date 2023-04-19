bf813ba1d11c1ad109d8bd66673749e0f225ef16
kungshang <kungshan@seed.net.tw>
2023-04-19 10:55:58 +0800

    PhotoImpact X3
    本安裝軟體有兩個批次檔"Data1.cab.bat"，分別在"\PhotoImpact X3"與"\PhotoImpact X3\Corel MediaOne"資料夾當中，安裝前請先執行，以進行大型檔案的合併。

---
+++ PhotoImpact X3/Corel MediaOne/Data1.cab.bat
@@ -0,0 +1,1 @@
+copy/b "Data1.cab.WinmendSp0"+"Data1.cab.WinmendSp1" "Data1.cab"
---
+++ PhotoImpact X3/Data1.cab.bat
@@ -0,0 +1,1 @@
+copy/b "Data1.cab.WinmendSp0"+"Data1.cab.WinmendSp1"+"Data1.cab.WinmendSp2"+"Data1.cab.WinmendSp3"+"Data1.cab.WinmendSp4" "Data1.cab"
